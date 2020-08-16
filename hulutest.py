b877f78a20724c6d764f6fc64715c21da86222e3
Zach <boxcarracer0110@gmail.com>
2020-08-16 01:01:03 -0700

    hulu changed the way episodes were tagged, now uses action chains with an offset to click episode choice

--- hulutest.py
+++ hulutest.py
@@ -15,10 +15,11 @@ options.add_argument("user-data-dir=C:\\Users\\Boxca\\AppData\\Local\\Google\\Ch
 driver = webdriver.Chrome(r"C:\Users\Boxca\Downloads\Drivers\chromedriver_win32 (1)\chromedriver.exe", options=options)
 
 
+
 driver.get('https://www.hulu.com/search')
-driver.find_element_by_css_selector('.cu-globalnav-search').click()
+driver.find_element_by_xpath("//input[@placeholder='Search']").click()
 search_box = WebDriverWait(driver, 4).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "cu-search-input"))
+    EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search']"))
 )
 
 
@@ -27,10 +28,14 @@ search_query = input('What do you want to watch?')
 
 search_box.send_keys(search_query)
 
-time.sleep(1)
+#time.sleep(1)
+
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li.InstantSearch__Option a")))
+
 search_result_count = len(driver.find_elements_by_css_selector('li.InstantSearch__Option a'))
+
 print(
-    f"Please enter choice from 1 to {search_result_count}:") if search_result_count else print(
+    f"Please enter choice from 1 to 3:") if search_result_count else print(
     "No result Found")
 
 for num, element in enumerate(driver.find_elements_by_css_selector('li.InstantSearch__Option a'),
@@ -41,7 +46,7 @@ for num, element in enumerate(driver.find_elements_by_css_selector('li.InstantSe
 
 choice = int(input())
 if search_result_count < choice:
-    print(f"Please enter only digits from 1 to {num}")
+    print(f"Please enter only digits from 1 to 3")
 
 javaScript = f"document.querySelectorAll('li.InstantSearch__Option a')[{choice - 1}].click();"
 driver.execute_script(javaScript)
@@ -49,8 +54,8 @@ driver.execute_script(javaScript)
 
 def has_seasons():
     try:
-        time.sleep(1)
-        season_dropdown = driver.find_element_by_css_selector('button.Select__control')
+        time.sleep(.5)
+        driver.find_element_by_css_selector('button.Select__control')
         return True
 
     except NoSuchElementException:
@@ -61,9 +66,9 @@ print(f"Seasons found: {has_seasons()}")
 
 def has_episodes():
     try:
-        time.sleep(1)
+        time.sleep(.5)
         #episodes = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#Episodes")))
-        episodes = driver.find_element_by_css_selector("#Episodes")
+        driver.find_element_by_css_selector("#Episodes")
         return True
 
     except NoSuchElementException:
@@ -106,8 +111,7 @@ elif has_seasons() == True:
     for num, element in enumerate(seasons_list, start=1):
         print(f"Enter {num} for {element.text}")
 
-    input(f"Please enter choice from 1 to {seasons_count}:")
-    choice = int(input())
+    choice = int(input(f"Please enter choice from 1 to {seasons_count}:"))
     if seasons_count < choice:
         print(f"Please enter only digits from 1 to {seasons_count}")
 
@@ -118,43 +122,30 @@ elif has_seasons() == True:
     def get_episodes():
         global episodes_exist
         episodes_exist = WebDriverWait(driver, 2).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "button.HoverSetup__button")))
+            EC.element_to_be_clickable((By.CLASS_NAME, "StandardEmphasisHorizontalTileContent__prompt")))
         return WebDriverWait(driver, 5).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "button.HoverSetup__button")))
-
-
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "StandardEmphasisHorizontalTileContent__prompt")))
 
     episode_list = get_episodes()
     episode_count = len(episode_list)
 
-
-
     for num, element in enumerate(episode_list, start=1):
-        print(f"Enter {num} for episode {num}")
-
+        print(f"Enter {num} for Episode {num}")
 
     choice = int(input())
 
     if episode_count < choice:
         print(f"Please enter only digits from 1 to {num}")
 
-
-    driver.find_element_by_xpath("//button[@class='Select__control']").click()
-    time.sleep(1)
-    driver.find_element_by_xpath("//div[@class='Details__collection']").click()
-
-
-
-    driver.find_elements_by_css_selector("button.HoverSetup__button")[choice - 1].click()
-
-
-
-
-    # episodes = driver.find_elements_by_css_selector("button.HoverSetup__button")
-    # for num, element in enumerate(episodes):
-    #     print(f"Enter {num} for {element.text}")
-    #     if num == 5:
-    #         print('not loading anymore')
+    # Hulu changed the way the play button element was identified.
+    # This is the workaround to to select episode choice.
+    episode = driver.find_elements_by_class_name("StandardEmphasisHorizontalTileContent__prompt")[choice - 1]
+    print(episode.text)
+    print(episode.location)
+    action = webdriver.common.action_chains.ActionChains(driver)
+    action.move_to_element_with_offset(episode, 25, -25)
+    action.click()
+    action.perform()
 
 
 else:
@@ -163,9 +154,9 @@ else:
     def get_episodes():
         global episodes_exist
         episodes_exist = WebDriverWait(driver, 2).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "button.HoverSetup__button")))
+            EC.element_to_be_clickable((By.CLASS_NAME, "StandardEmphasisHorizontalTileContent__prompt")))
         return WebDriverWait(driver, 5).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "button.HoverSetup__button")))
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "StandardEmphasisHorizontalTileContent__prompt")))
 
     get_episodes()
     episode_list = get_episodes()
@@ -179,4 +170,12 @@ else:
     if episode_count < choice:
         print(f"Please enter only digits from 1 to {episode_count}")
 
-    driver.find_elements_by_css_selector("button.HoverSetup__button")[choice - 1].click()
+    # Hulu changed the way the play button element was identified.
+    # This is the workaround to to select episode choice.
+    episode = driver.find_elements_by_class_name("StandardEmphasisHorizontalTileContent__prompt")[choice - 1]
+    print(episode.text)
+    print(episode.location)
+    action = webdriver.common.action_chains.ActionChains(driver)
+    action.move_to_element_with_offset(episode, 25, -25)
+    action.click()
+    action.perform()
