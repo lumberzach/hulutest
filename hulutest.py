1e6273b4fd47c1f51c55d40e6b14c5663829038d
Zach <boxcarracer0110@gmail.com>
2020-08-17 23:04:50 -0700

    created function for playing selected episode, cleaned some code to be cleaner/shorter

--- hulutest.py
+++ hulutest.py
@@ -8,30 +8,24 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-# loading webdrivers and opening the website
+# Setting options and path to your chrome profile.
 options = Options()
-# Path to your chrome profile
 options.add_argument("user-data-dir=C:\\Users\\Boxca\\AppData\\Local\\Google\\Chrome\\profiletwo")
 driver = webdriver.Chrome(r"C:\Users\Boxca\Downloads\Drivers\chromedriver_win32 (1)\chromedriver.exe", options=options)
 
-
-
+# Opening hulu to the search page
 driver.get('https://www.hulu.com/search')
 driver.find_element_by_xpath("//input[@placeholder='Search']").click()
 search_box = WebDriverWait(driver, 4).until(
     EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search']"))
 )
 
-
+# Taking user's search string
 search_query = input('What do you want to watch?')
-
-
 search_box.send_keys(search_query)
 
-#time.sleep(1)
-
+# Gives user first 3 results and allows selection.
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li.InstantSearch__Option a")))
-
 search_result_count = len(driver.find_elements_by_css_selector('li.InstantSearch__Option a'))
 
 print(
@@ -52,6 +46,7 @@ javaScript = f"document.querySelectorAll('li.InstantSearch__Option a')[{choice -
 driver.execute_script(javaScript)
 
 
+# Checking if the selection is a series with more than one season.
 def has_seasons():
     try:
         time.sleep(.5)
@@ -60,43 +55,60 @@ def has_seasons():
 
     except NoSuchElementException:
         return False
+
+
 print(f"Seasons found: {has_seasons()}")
-#print(has_seasons())
 
 
+# Checking if the selection is a series.
 def has_episodes():
     try:
         time.sleep(.5)
-        #episodes = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#Episodes")))
         driver.find_element_by_css_selector("#Episodes")
         return True
 
     except NoSuchElementException:
         return False
+
+
 print(f"Episodes found: {has_episodes()}")
 
 
-
+# Checking if the selection is a movie.
 def is_movie():
-    if has_episodes() == False:
+    if not has_episodes():
         return True
-
     else:
         return False
+
+
 print(f"Movie: {is_movie()}")
 
 
-# Playing the selection
+# Hulu changed the way the play button element was identified.
+# This is the workaround to to select episode choice.
+def play_episode():
+    episode = driver.find_elements_by_class_name("StandardEmphasisHorizontalTileContent__prompt")[choice - 1]
+    print(episode.text)
+    print(episode.location)
+    action = webdriver.common.action_chains.ActionChains(driver)
+    action.move_to_element_with_offset(episode, 25, -25)
+    action.click()
+    action.perform()
 
-if is_movie() == True:
+
+# Playing the selection
+if is_movie():
     print("It's a movie, lets play it")
     driver.find_element_by_xpath("//a[@class='WatchAction__btn']").click()
 
-elif has_seasons() == True:
+
+# If selection has seasons, we find which season user would like to choose.
+elif has_seasons():
     print("It's a series with more than one season, let's ask which season")
 
+    # Finds out how many seasons available
     def get_seasons():
-        global season_dropdown
         season_dropdown = WebDriverWait(driver, 2).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "button.Select__control"))
         )
@@ -118,7 +130,7 @@ elif has_seasons() == True:
     time.sleep(1)
     driver.find_elements_by_css_selector('.Select__menu-list li')[choice - 1].click()
 
-
+    # Finds out how many episodes are in the given season
     def get_episodes():
         global episodes_exist
         episodes_exist = WebDriverWait(driver, 2).until(
@@ -133,21 +145,13 @@ elif has_seasons() == True:
         print(f"Enter {num} for Episode {num}")
 
     choice = int(input())
+    play_episode()
 
     if episode_count < choice:
         print(f"Please enter only digits from 1 to {num}")
 
-    # Hulu changed the way the play button element was identified.
-    # This is the workaround to to select episode choice.
-    episode = driver.find_elements_by_class_name("StandardEmphasisHorizontalTileContent__prompt")[choice - 1]
-    print(episode.text)
-    print(episode.location)
-    action = webdriver.common.action_chains.ActionChains(driver)
-    action.move_to_element_with_offset(episode, 25, -25)
-    action.click()
-    action.perform()
 
-
+# Selecting an episode if it is a series with only one season.
 else:
     print("It's a series with only one season")
 
@@ -166,16 +170,7 @@ else:
         print(f"Enter {num} for episode {num}")
 
     choice = int(input())
+    play_episode()
 
     if episode_count < choice:
         print(f"Please enter only digits from 1 to {episode_count}")
-
-    # Hulu changed the way the play button element was identified.
-    # This is the workaround to to select episode choice.
-    episode = driver.find_elements_by_class_name("StandardEmphasisHorizontalTileContent__prompt")[choice - 1]
-    print(episode.text)
-    print(episode.location)
-    action = webdriver.common.action_chains.ActionChains(driver)
-    action.move_to_element_with_offset(episode, 25, -25)
-    action.click()
-    action.perform()
